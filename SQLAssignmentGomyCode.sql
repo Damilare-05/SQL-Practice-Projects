781dacaad4afbbfdc877d1b053a01fb78c59899e
Talabi Damilare Hannah <damilaretalabi9@gmail.com>
2024-05-14 12:10:11 +0100

    Update SQLAssignmentGomyCode.sqls

--- SQLAssignmentGomyCode.sql
+++ SQLAssignmentGomyCode.sql
@@ -1,19 +1,19 @@
 
 CREATE TABLE Customers(
-customers_id INTEGER PRIMARY KEY NOT NULL,
+customers_id INTEGER PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 address VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE products(
-product_id INTEGER PRIMARY KEY NOT NULL,
+product_id INTEGER PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 price DECIMAL CHECK(price > 0) NOT NULL
 );
 
 
 CREATE TABLE Orders(
-order_id INTEGER PRIMARY KEY NOT NULL,
+order_id INTEGER PRIMARY KEY,
 product_id INT NOT NULL,
 customers_id INT NOT NULL,
 quantity INTEGER NOT NULL,
