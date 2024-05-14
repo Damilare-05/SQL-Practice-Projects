ec8a1291c2bcda39e34c1ee47cae2cd07a98b1a8
Talabi Damilare Hannah <damilaretalabi9@gmail.com>
2024-05-14 13:31:21 +0100

    Update SQLQueryPractice.sql

--- SQLQueryPractice.sql
+++ SQLQueryPractice.sql
@@ -1,11 +1,13 @@
+--create a database
 CREATE DATABASE LibraryDB;
 GO
 
+--use the database	
 USE LibraryDB;
 GO
 
 
-
+--creating tables
 CREATE TABLE Authors(
    author_id INT PRIMARY KEY,
    author_name VARCHAR(50),
@@ -31,5 +33,27 @@ CREATE TABLE Books(
 	FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
+--altering or adding constraints
 
-SELECT * FROM Books;
+ALTER TABLE Books 
+ADD CONSTRAINT fk_authorID FOREIGN KEY (author_id) REFERENCES Authors(author_id) 
+
+ALTER TABLE Books
+ADD CONSTRAINT chk_publication_year CHECK (publication_year < 2000);
+
+ALTER TABLE Authors
+ADD CONSTRAINT chk_birth_year CHECK (birth_year < 2000);
+
+---delete books table
+DROP TABLE Books;
+
+--Recreate table for books
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY,
+	title VARCHAR(50),
+	author VARCHAR(50),
+	publication_year INT CHECK (publication_year < 2000), 
+	ISBN VARCHAR(50),
+	author_id INT, 
+	FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
