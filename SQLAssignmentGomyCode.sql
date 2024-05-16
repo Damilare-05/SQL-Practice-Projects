b0d1d817c115fd6e42d3a08b07c7a2f7cba3fad9
Talabi Damilare Hannah <damilaretalabi9@gmail.com>
2024-05-16 13:23:13 +0100

    Update SQLAssignmentGomyCode.sql

--- SQLAssignmentGomyCode.sql
+++ SQLAssignmentGomyCode.sql
@@ -1,17 +1,20 @@
+USE MY_DB;
 
+--Creating customer table
 CREATE TABLE Customers(
 customers_id INTEGER PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 address VARCHAR(50) NOT NULL
 );
 
+--creating products table
 CREATE TABLE products(
-product_id INTEGER PRIMARY KEY,
+product_id INTEGER PRIMARY KEY ,
 name VARCHAR(50) NOT NULL,
-price DECIMAL(4, 2) CHECK(price > 0) NOT NULL
+price DECIMAL(5, 2) CHECK(price > 0) NOT NULL
 );
 
-
+--creating orders table
 CREATE TABLE Orders(
 order_id INTEGER PRIMARY KEY,
 product_id INT NOT NULL,
@@ -23,3 +26,25 @@ FOREIGN KEY (customers_id) REFERENCES Customers(customers_id),
 FOREIGN KEY (product_id) REFERENCES Products(product_id) 
 );
 GO
+
+--inserting data into the products table
+INSERT INTO products(product_id, name, price) 
+VALUES (1, 'Cookies', 10 ), 
+       (2, 'Candy', 5.2) ;
+
+--inserting data into customers table
+INSERT INTO Customers(customers_id, name, address) 
+VALUES (1, 'Ahmed', 'Tunisia' ), 
+       (2, 'Coulibaly', 'Senegal'),
+	   (3, 'Hasan', 'Egypt');
+
+--inserting data into products table
+INSERT INTO Orders(order_id, customers_id, product_id, quantity, order_date) 
+VALUES (1, 1, 2, 3, '2023-01-22' ), 
+       (2, 2, 1, 10, '2023-04-14') ;
+
+SELECT * FROM Customers;
+
+SELECT * FROM Orders;
+
+SELECT * FROM products;
